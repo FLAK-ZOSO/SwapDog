86f17ba15c461d0faba3f68856ee50850f6cf81e
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2025-08-19 18:33:56 +0300

    Simplified class syntax, improved styling, error handling on busy swap

--- swapdog.py
+++ swapdog.py
@@ -14,16 +14,13 @@ PERIOD = 1
 
 
 class Threshold:
-    percentage: float
-    swap: str
-    enabled: bool
 
     def __init__(self, percentage: float, swap: str):
-        object.__setattr__(self, "percentage", percentage)
-        object.__setattr__(self, "swap", os.path.realpath(swap))
+        self.percentage = percentage
+        self.swap = os.path.realpath(swap)
     
     def __repr__(self):
-        return f"<Treshold at {self.percentage}% for {self.swap}>"
+        return f"<Threshold at {self.percentage}% for {self.swap}>"
 
 
 def read_configuration(path: str) -> tuple[list[Threshold], float]:
@@ -40,13 +37,19 @@ def read_configuration(path: str) -> tuple[list[Threshold], float]:
         return (thresholds, config["period"])
     return (thresholds, PERIOD)
 
+
 def list_enabled_swaps() -> list[bytes]:
     return subprocess.check_output([
         "swapon", "--show=NAME", "--raw", "--noheadings"
     ]).splitlines()
 
+
 def enable_swap(swap: str) -> None:
-    subprocess.check_call(["swapon", swap])
+    try:
+        subprocess.check_call(["swapon", swap])
+    except subprocess.CalledProcessError as e:
+        print(f"Error enabling swap {swap}: {e}", file=sys.stderr)
+
 
 if __name__ == '__main__':
     thresholds, period = read_configuration(sys.argv[1] if len(sys.argv) > 1 else CONFIG)
@@ -55,7 +58,6 @@ if __name__ == '__main__':
         current = psutil.virtual_memory().percent
         print(current, file=sys.stderr)
         enabled_swaps = map(lambda x: x.decode('utf-8'), list_enabled_swaps())
-        print(enabled_swaps, file=sys.stderr)
         for t in thresholds:
             print(t, file=sys.stderr)
             if current >= t.percentage:
